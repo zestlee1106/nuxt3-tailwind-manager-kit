dfc83130b19c131cab7ecb46e1318a06fe6b0245
ltg <ltg@bbangyanetworks.com>
2022-12-26 23:23:18 +0900

    config.sh 경로 수정

--- scripts/deploy/application_stop.sh
+++ scripts/deploy/application_stop.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 환경파일 로드
-source /home/ec2-user/hntap/dcr-front/scripts/deploy/config.sh
+source /home/ec2-user/app/scripts/deploy/config.sh
 
 docker stop ${APP_NAME}
 docker rm ${APP_NAME}
